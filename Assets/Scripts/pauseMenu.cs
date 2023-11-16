1ac59e50597118f6a01e1b93976bc23cd0279405
wiwechen <129801256+wiwechen@users.noreply.github.com>
2023-11-16 12:44:09 -0800

    created pause menu, still need to work on buttons

---
+++ Assets/Scripts/pauseMenu.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class pauseMenu : MonoBehaviour
+{
+    public static bool gamePause;
+    [SerializeField] GameObject screen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gamePause = false;
+        screen.SetActive(false);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            gamePause = !gamePause;
+            pauseGame();
+        }
+    }
+
+    void pauseGame()
+    {
+        if (gamePause)
+        {
+            Time.timeScale = 0f;
+            screen.SetActive(true);
+        }
+        else
+        {
+            Time.timeScale = 1f;
+            screen.SetActive(false);
+        }
+    }
+}
