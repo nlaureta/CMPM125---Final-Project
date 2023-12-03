4a22d3ba6b164bb9368f2640a55c283be6c865a8
nlaureta <73408370+nlaureta@users.noreply.github.com>
2023-12-02 20:59:05 -0800

    fixed position reset bug for player 1
    player now resets back to initial position after each round

--- Assets/Scripts/RoundManager.cs
+++ Assets/Scripts/RoundManager.cs
@@ -12,7 +12,7 @@ public class RoundManager : MonoBehaviour
     [SerializeField] private Slider enemyHealthbar;
     [SerializeField] private Slider playerHealthBar;
     [SerializeField] private GameObject gameOverScreen;
-    
+
     public Toggle[] player1Toggles;
     public Toggle[] player2Toggles;
     public GameObject player1WinText;
@@ -21,12 +21,13 @@ public class RoundManager : MonoBehaviour
     public Timer time;
 
 
- 
+
     private static RoundManager _instance;
     public static RoundManager Instance { get { return _instance; } }
 
-    void Awake() {
-         if (_instance != null && _instance != this)
+    void Awake()
+    {
+        if (_instance != null && _instance != this)
         {
             Destroy(this.gameObject);
         }
@@ -34,7 +35,12 @@ public class RoundManager : MonoBehaviour
         {
             _instance = this;
         }
-       
+
+    }
+
+    IEnumerator StartNewRoundDelay(){
+        yield return new WaitForSeconds(3); // wait for 3 seconds
+        StartNewRound();
     }
     // Start is called before the first frame update
     void Start()
@@ -46,88 +52,101 @@ public class RoundManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void RoundEnd(int winningPlayerIndex)
     {
-        if (winningPlayerIndex != -1) { // round timer not 0
+        if (winningPlayerIndex != -1)
+        { // round timer not 0
             Debug.Log("Player " + (winningPlayerIndex + 1) + " wins the round!");
-            if (winningPlayerIndex == 0) {
+            if (winningPlayerIndex == 0)
+            {
                 //Debug.Log(player1Wins);
-                if(player1Wins < 2)
+                if (player1Wins < 2)
                     player1Toggles[player1Wins].isOn = true;
                 player1WinText.SetActive(true);
                 //P1R1.color = Color.green;
                 player1Wins++;
             }
-            else { 
-                if(player2Wins < 2)
+            else
+            {
+                if (player2Wins < 2)
                     player2Toggles[player2Wins].isOn = true;
                 player2WinText.SetActive(true);
                 player2Wins++;
             }
 
-        }else{
-            if(HealthManager.Instance.player1Health < HealthManager.Instance.player2Health) {
+        }
+        else
+        {
+            if (HealthManager.Instance.player1Health < HealthManager.Instance.player2Health)
+            {
                 Debug.Log("Time's up! Player2 has won");
                 player2WinText.SetActive(true);
                 player2Wins++;
                 //StartNewRound();
-                StartCoroutine(StartNewRound());
-            }else if(HealthManager.Instance.player1Health > HealthManager.Instance.player2Health) {
+                StartCoroutine(StartNewRoundDelay());
+            }
+            else if (HealthManager.Instance.player1Health > HealthManager.Instance.player2Health)
+            {
                 Debug.Log("Time's up! Player1 has won");
                 player1WinText.SetActive(true);
                 player1Wins++;
                 //StartNewRound();
-                StartCoroutine(StartNewRound());
-            }else {
+                StartCoroutine(StartNewRoundDelay());
+            }
+            else
+            {
                 Debug.Log("Round tied! Resuming with a new round.");
                 //StartNewRound();
                 drawText.SetActive(true);
-                StartCoroutine(StartNewRound());
+                StartCoroutine(StartNewRoundDelay());
             }
-             //StartCoroutine(StartNewRound());
+            //StartCoroutine(StartNewRound());
         }
         //HealthManager.Instance.FlagPrint();
-        if (player1Wins == roundsToWin || player2Wins == roundsToWin) { //determines who won best of 3
-               
+        if (player1Wins == roundsToWin || player2Wins == roundsToWin)
+        { //determines who won best of 3
+
             Debug.Log("Game Over! Player " + (player1Wins >= roundsToWin ? 1 : 2) + " wins the game!");
             // Game Over UI here
             gameOverScreen.SetActive(true);
             Time.timeScale = 0f;
 
-                
-
-                //HealthManager.Instance.FlagPrint();
-                //StartNewRound();
-                //StartCoroutine(StartNewRound());
-                //open menu UI here
-        }else{
+        }
+        else
+        {
             //StartNewRound();
             //Debug.Log("Test");
-            StartCoroutine(StartNewRound());
+            StartCoroutine(StartNewRoundDelay());
             Debug.Log("player1Wins is now: " + player1Wins);
         }
-       
+
     }
 
-    IEnumerator StartNewRound(){
-       //reset everything here
-       yield return new WaitForSeconds(3); // wait for 3 seconds
-       time.countdown = 90;
-       player1WinText.SetActive(false);
-       player2WinText.SetActive(false);
-       drawText.SetActive(false);
-       enemyHealthbar.value = 1;
-       playerHealthBar.value = 1;
-       GameObject.FindGameObjectWithTag("Player").transform.position = new Vector3(-4.59989977f,-1.95000005f,0.0179928206f);
-       GameObject.FindGameObjectWithTag("Enemy").transform.position = new Vector3(6.07310009f,-1.95000005f,0.0179928206f);
-       GameObject.FindGameObjectWithTag("Player").transform.localRotation = new Quaternion(0, 0, 0, 0);
-       GameObject.FindGameObjectWithTag("Enemy").transform.localRotation =  new Quaternion(0, 0, 0, 0);
-       HealthManager.Instance.players[0].currHealth = 100;
-       HealthManager.Instance.players[1].currHealth = 100;
-       HealthManager.Instance.ResetRoundEndFlag();
+    public void StartNewRound()
+    {
+        //reset everything here
+        //reset timer
+        time.countdown = 90;
+        //reset text
+        player1WinText.SetActive(false);
+        player2WinText.SetActive(false);
+        drawText.SetActive(false);
+        //reset healthbar
+        enemyHealthbar.value = 1;
+        playerHealthBar.value = 1;
+        //reset player positions
+        GameObject.FindGameObjectWithTag("Player 1").transform.position = new Vector3(-4.59989977f, -1.95000005f, 0.0179928206f);
+        GameObject.FindGameObjectWithTag("Enemy").transform.position = new Vector3(6.07310009f, -1.95000005f, 0.0179928206f);
+        GameObject.FindGameObjectWithTag("Player 1").transform.localRotation = new Quaternion(0, 0, 0, 0);
+        GameObject.FindGameObjectWithTag("Enemy").transform.localRotation = new Quaternion(0, 0, 0, 0);
+        //reset player health value
+        HealthManager.Instance.players[0].currHealth = 100;
+        HealthManager.Instance.players[1].currHealth = 100;
+        //reset round boolean
+        HealthManager.Instance.ResetRoundEndFlag();
     }
 
 }
